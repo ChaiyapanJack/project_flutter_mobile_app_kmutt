12c52b3e5f1da8da1fe316d887cafecaf2afd10d
Chaiyapankmutt65 <125781241+Chaiyapankmutt65@users.noreply.github.com>
2023-02-21 21:39:07 +0700

    Code From Git

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'auth_gate.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,8 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       home: const AuthGate(),
     );
