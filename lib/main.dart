9141a091626e2c2ad475507def60b6f018bdcd35
Chaiyapankmutt65 <125781241+Chaiyapankmutt65@users.noreply.github.com>
2023-02-22 19:23:27 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
-import 'app_state.dart';
+
 import 'login_signup_page.dart';
 
 import 'package:provider/provider.dart'; // new
-import 'app_state.dart';
 
 void main() {
   // Modify from here...
