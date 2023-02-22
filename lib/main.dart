cb8478d3f139c0874909d6a031f75294e2219a1e
Chaiyapankmutt65 <125781241+Chaiyapankmutt65@users.noreply.github.com>
2023-02-22 09:44:28 +0700

    Ui login success

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,15 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // ignore: unnecessary_new
     return new MaterialApp(
         title: 'Flutter Login Demo',
         theme: new ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: new LoginSignupPage());
+        home: MyLoginPage(
+          key: null,
+        ));
   }
 }
 
