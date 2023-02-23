8e1b583c1be0285f3e422267b0002d554a3849a2
Chaiyapankmutt65 <125781241+Chaiyapankmutt65@users.noreply.github.com>
2023-02-23 16:06:02 +0700

    singout

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:project_flutter_mobile_app_kmutt/login_signup_page.dart';
 
 class MyHomePage extends StatefulWidget {
   //final Firebase user;
@@ -19,10 +20,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
     return Scaffold(
         appBar: AppBar(
           title: Text("My Firebase App", style: TextStyle(color: Colors.white)),
           backgroundColor: Color.fromARGB(255, 183, 236, 123),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.exit_to_app),
+                color: Colors.white,
+                onPressed: () {
+                  signOut(context);
+                })
+          ],
         ),
         body: Container(
             child: Center(
@@ -32,4 +42,13 @@ class _MyHomePageState extends State<MyHomePage> {
           //Text(widget.user.email, style: TextStyle(fontSize: 16)),
         ]))));
   }
+}
+
+void signOut(BuildContext context) {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  _auth.signOut();
+  Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => MyLoginPage()),
+      ModalRoute.withName('/'));
 }
